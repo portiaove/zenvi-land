fb64003f8df13bc087be2701a33618a5be52436a
portiaove <juanportillog@gmail.com>
2024-06-23 18:29:04 +0200

    Add card shuffler functionality

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,21 @@ export const ROUTES = {
   PRIVACY_POLICY: "#",
   LEGAL: "#",
 };
+
+export const CARDS_CONTENT = [
+  {
+    title: "Crea una cuenta",
+    subtitle:
+      "Te pediremos tus datos personales y NIF, esto lo necesitamos para vincular tus datos a tu contrato",
+  },
+  {
+    title: "Datos de tu dirección",
+    subtitle:
+      "Te pediremos los datos de tu dirección, con ello podremos calcular cuanto puedes ahorrar",
+  },
+  {
+    title: "Calculadora y IBAN",
+    subtitle:
+      "Te pediremos el IBAN para realizar el cambio de contrato automático",
+  },
+];
